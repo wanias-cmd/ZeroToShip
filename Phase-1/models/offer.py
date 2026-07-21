bff80f957e90595ebec830742347d08a59392d4b
Wania Sheeba <waniasartworld@gmail.com>
2026-07-21 16:20:33 +0500

    Phase 1 Complete: Establish core TradePost and Offer models alongside dictionary JSON serialization pipelines

---
+++ Phase-1/models/offer.py
@@ -0,0 +1,26 @@
+class NegotiationOffer:
+    def __init__(self, offer_id, post_id, proposer_id, offered_item_details, turn_holder_id):
+        self.offer_id = offer_id
+        self.post_id = post_id
+        self.proposer_id = proposer_id
+        self.offered_item_details = offered_item_details
+        self.turn_holder_id = turn_holder_id
+
+    def to_dict(self):
+        return {
+            "offer_id": self.offer_id,
+            "post_id": self.post_id,
+            "proposer_id": self.proposer_id,
+            "offered_item_details": self.offered_item_details,
+            "turn_holder_id": self.turn_holder_id
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            offer_id=data["offer_id"],
+            post_id=data["post_id"],
+            proposer_id=data["proposer_id"],
+            offered_item_details=data["offered_item_details"],
+            turn_holder_id=data["turn_holder_id"]
+        )
